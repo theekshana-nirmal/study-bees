023192c8296d5f47a9d3f111913331e43d29bc94
Theekshana Nirmal <theekshanab1234@gmail.com>
2024-07-02 17:07:21 +0530

    logo added

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,8 @@
 <header class="navbar-shadow">
     <div class="container">
         <div class="logo logo-s">
-            
+            <img src="<?php echo htmlspecialchars($navbar_logo_image_path); ?>" alt="LOGO" class="logo-small">
+            <h1>Study Bees</h1>
         </div>
         <nav>
             <a href="<?php echo htmlspecialchars($login_page_path); ?>" class="login-link">Log in</a>
